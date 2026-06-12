e06493844381b4d7842e9e495b6ee14734429f8d
iSuleymanCoban <suleymanc756@gmail.com>
2026-06-13 00:25:42 +0300

    Add files via upload

--- veritabani.py
+++ veritabani.py
@@ -4,13 +4,15 @@ import random
 class Veritabani:
     
     def __init__(self):
+        # check_same_thread=False allows Streamlit's multiple threads to use the same connection
         self.baglanti = sqlite3.connect("banka.db", check_same_thread=False)
-        self.imlec = self.baglanti.cursor()
+        # Tabloları oluştur
         self.kurtablo()
 
     def kurtablo(self):
+        cursor = self.baglanti.cursor()
         # Kullanıcıların bilgilerini tutacağımız tablo
-        self.imlec.execute('''
+        cursor.execute('''
             CREATE TABLE IF NOT EXISTS kullanicilar (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 isim TEXT,
@@ -22,7 +24,7 @@ class Veritabani:
         ''')
         
         # İşlem geçmişini tutacağımız tablo
-        self.imlec.execute('''
+        cursor.execute('''
             CREATE TABLE IF NOT EXISTS islemler (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 hesap_no TEXT,
@@ -35,17 +37,19 @@ class Veritabani:
 
     # Rastgele hesap numarası üreten fonksiyon
     def hesapnocreate(self):
+        cursor = self.baglanti.cursor()
         while True:
             rastgele_sayi = str(random.randint(10000000, 99999999))
-            self.imlec.execute("SELECT * FROM kullanicilar WHERE hesap_no=?", (rastgele_sayi,))
-            sonuc = self.imlec.fetchone()
+            cursor.execute("SELECT * FROM kullanicilar WHERE hesap_no=?", (rastgele_sayi,))
+            sonuc = cursor.fetchone()
     
             if sonuc == None:
                 return rastgele_sayi
 
     # Para yatırma/çekme gibi işlemleri kaydeden fonksiyon
     def logtut(self, hesap_no, islem_tipi, miktar):
-        self.imlec.execute(
+        cursor = self.baglanti.cursor()
+        cursor.execute(
             "INSERT INTO islemler (hesap_no, islem_tipi, miktar) VALUES (?, ?, ?)",
             (hesap_no, islem_tipi, miktar)
         )
@@ -53,31 +57,33 @@ class Veritabani:
 
     # Yeni bir kullanıcı ekleyen fonksiyon
     def userekle(self, isim, soyisim, sifre):
+        cursor = self.baglanti.cursor()
         # Önce rastgele bir hesap numarası alıyoruz
         yeni_hesap_no = self.hesapnocreate()
         
-        self.imlec.execute(
+        cursor.execute(
             "INSERT INTO kullanicilar (isim, soyisim, hesap_no, sifre) VALUES (?, ?, ?, ?)",
             (isim, soyisim, yeni_hesap_no, sifre)
         )
         self.baglanti.commit()
         
-        
         return yeni_hesap_no
 
     # Giriş yaparken hesap no ve şifre doğru mu diye kontrol eden fonksiyon
     def userkontrol(self, hesap_no, sifre):
-        self.imlec.execute(
+        cursor = self.baglanti.cursor()
+        cursor.execute(
             "SELECT * FROM kullanicilar WHERE hesap_no=? AND sifre=?",
             (hesap_no, sifre)
         )
         # Eğer doğruysa kullanıcının bilgilerini, yanlışsa None döndürür
-        return self.imlec.fetchone()
+        return cursor.fetchone()
 
     # Hesaptaki parayı getiren fonksiyon
     def paracekv(self, hesap_no):
-        self.imlec.execute("SELECT bakiye FROM kullanicilar WHERE hesap_no=?", (hesap_no,))
-        sonuc = self.imlec.fetchone()
+        cursor = self.baglanti.cursor()
+        cursor.execute("SELECT bakiye FROM kullanicilar WHERE hesap_no=?", (hesap_no,))
+        sonuc = cursor.fetchone()
         
         if sonuc != None:
             return sonuc[0] # sonucun içindeki ilk veri bakiyedir
@@ -86,7 +92,8 @@ class Veritabani:
 
     # Para yatırma/çekme işlemi sonrası yeni bakiyeyi güncelleyen fonksiyon
     def moneyreset(self, hesap_no, yeni_bakiye):
-        self.imlec.execute(
+        cursor = self.baglanti.cursor()
+        cursor.execute(
             "UPDATE kullanicilar SET bakiye=? WHERE hesap_no=?",
             (yeni_bakiye, hesap_no)
         )
@@ -94,8 +101,9 @@ class Veritabani:
 
     # Sadece hesap numarasını kullanarak kullanıcının adını/soyadını bulduğumuz fonksiyon
     def hesapbul(self, hesap_no):
-        self.imlec.execute("SELECT * FROM kullanicilar WHERE hesap_no=?", (hesap_no,))
-        return self.imlec.fetchone()
+        cursor = self.baglanti.cursor()
+        cursor.execute("SELECT * FROM kullanicilar WHERE hesap_no=?", (hesap_no,))
+        return cursor.fetchone()
 
     # Program kapanırken veritabanı bağlantısını kapatan fonksiyon
     def sistemi_kapat(self):
